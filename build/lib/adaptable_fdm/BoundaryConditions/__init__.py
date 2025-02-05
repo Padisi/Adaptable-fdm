3b9ab8cdd7dfeb298c1f3763b27d228b6c045213
Padisi <pablodiezsilva@gmail.com>
2025-02-05 11:08:25 +0100

    Added gitignore

--- build/lib/adaptable_fdm/BoundaryConditions/__init__.py
+++
@@ -1,6 +0,0 @@
-from .BoundaryCondition import BoundaryCondition
-from .Periodic import Periodic, AntiPeriodic
-from .Dirichlet import Dirichlet
-from .Neumann import Neumann,NeumannFlatX,NeumannFlatY,NeumannFlatZ
-
-__all__ = ["BoundaryCondition","Periodic","AntiPeriodic","Dirichlet","Neumann","NeumannFlatX","NeumannFlatY","NeumannFlatZ"]
--- build/lib/adaptable_fdm/Integrators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Poisson3D import Poisson3D
-from .Poisson3D_eps import Poisson3D_eps
-
-__all__ = ["Poisson3D","Poisson3D_eps"]
