8c1659e320c0de8b39e7d1f4d1de7a3f5f25286a
Padisi <pablodiezsilva@gmail.com>
2025-12-01 17:44:30 +0100

    Added inhomogeneus import

--- adaptable_fdm/Integrators/__init__.py
+++ adaptable_fdm/Integrators/__init__.py
@@ -1,6 +1,5 @@
 from .IntegratorBase import IntegratorBase
-from .Poisson3D import Poisson3D, Poisson3D_eps
-from .Poisson3D import Poisson3D_eps
+from .Poisson3D import Poisson3D, Poisson3D_eps, Poisson3D_Inhomogeneous
 from .Tester import Tester
 
-__all__ = ["Poisson3D","Poisson3D_eps","Tester"]
+__all__ = ["Poisson3D","Poisson3D_eps", "Poisson3D_Inhomogeneous","Tester"]
